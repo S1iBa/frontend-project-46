74d26360a1f161baafa90a52843e3b1e3bf39063
Daria <daryaab321@gmail.com>
2024-01-25 21:23:24 +0200

    fix lint2

--- src/formatter/tree.js
+++ src/formatter/tree.js
@@ -1,10 +1,12 @@
+import _ from 'lodash';
+
 const buildTree = (file1, file2) => {
   const iter = (data1, data2) => {
     const keys1 = Object.keys(data1);
     const keys2 = Object.keys(data2);
 
     const uniqKeys = Array.from(new Set(keys1.concat(keys2)));
-    const sortedKeys = [...uniqKeys].sort();
+    const sortedKeys = _.sort([...uniqKeys]);
 
     return sortedKeys.map((key) => {
       if (Object.hasOwn(data1, key) && !Object.hasOwn(data2, key)) {
